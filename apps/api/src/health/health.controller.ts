0e26310d2aab3c6b706b99d06701963a54e8fa74
Andrei Kotlyarenko <andrei.kotlyarenko@gmail.com>
2026-09-21 23:37:52 +0100

    feat: configure API documentation

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
-    @Get()
-    getHealth(): { status: string } {
-        return { status: 'ok' };
-    }
+  @Get()
+  @ApiOperation({ summary: 'Check API health' })
+  @ApiOkResponse({
+    description: 'The API is running',
+    schema: {
+      example: {
+        status: 'ok',
+      },
+    },
+  })
+  getHealth(): { status: string } {
+    return { status: 'ok' };
+  }
 }
